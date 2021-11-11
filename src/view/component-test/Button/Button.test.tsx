88b613832718e3c68b78bc01b3d0f8e8efe4e7fd
Abdulmoiz <abdulmoizahmer1996@gmail.com>
2021-11-11 12:09:33 -0700

    wrote component test for button

---
+++ src/view/component-test/Button/Button.test.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { render, screen, fireEvent } from "@testing-library/react";
+import { Button } from "../../index";
+
+test("Make sure button is rendering", () => {
+  render(<Button />);
+
+  const addButtonElement = screen.getByText("Add");
+
+  //and click to <add button>
+  fireEvent.click(addButtonElement);
+
+  expect(screen.getByText("Add")).toBeDisabled();
+});
