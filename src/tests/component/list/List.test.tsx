752262e753ff58051631219550e0f0e1a984d871
Abdulmoiz <abdulmoizahmer1996@gmail.com>
2021-11-11 20:43:25 -0700

    Updated: Todo list item test

--- src/tests/component/list/List.test.tsx
+++ src/tests/component/list/List.test.tsx
@@ -1,9 +1,16 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
 import { List } from "../../../view";
+import { TodoItem } from "../../../entity/TodoItem";
 
 test("Make sure list is rendering", () => {
-  render(<List />);
+  const items: TodoItem[] = [
+    TodoItem.fromJSON({
+      id: "618ddb6afe72881053259ea1",
+      description: "Buy some sugar",
+    }),
+  ];
+  render(<List items={items} />);
   const listElement = screen.getByRole("list");
   expect(listElement).toBeInTheDocument();
 });
