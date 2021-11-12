1732dbbde9e037ce4274372d0ab1a2832c442fbd
Abdulmoiz <abdulmoizahmer1996@gmail.com>
2021-11-12 12:51:46 -0700

    getting rid of pact tests

--- src/tests/contracts/jest-wrapper.ts
+++
@@ -1,9 +0,0 @@
-import globalAny from "./setup";
-
-beforeAll((done) => {
-  globalAny.provider.setup().then(() => done());
-});
-
-afterAll((done) => {
-  globalAny.provider.finalize().then(() => done());
-});
