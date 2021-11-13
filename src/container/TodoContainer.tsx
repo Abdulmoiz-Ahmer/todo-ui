3d6c01b4a93f36ec9ec5c1cd2be40f1ebd4f6fbc
Abdulmoiz <abdulmoizahmer1996@gmail.com>
2021-11-13 08:04:49 -0700

    ADDS: state for adding input in textbox

--- src/container/TodoContainer.tsx
+++ src/container/TodoContainer.tsx
@@ -2,17 +2,24 @@ import React, { useState } from "react";
 
 export const TodoContainer = () => {
   const [buttonDisabilityStatus, setButtonDisabilityStatus] = useState(false);
+  const [todoText, setTodoText] = useState<string>("");
 
   function handleButtonClick() {
     setButtonDisabilityStatus(true);
   }
 
+  function updateTodoText(todoText: string) {
+    setTodoText(todoText);
+  }
+
   return {
     state: {
       buttonDisabilityStatus,
+      todoText,
     },
     functions: {
       handleButtonClick,
+      updateTodoText,
     },
   };
 };
