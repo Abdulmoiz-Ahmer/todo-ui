4a80ce5c873f769c6a43b144da996758c725cdbc
Abdulmoiz <abdulmoizahmer1996@gmail.com>
2021-11-15 22:54:14 -0700

    ADDS: unit test for handleButton

---
+++ src/tests/unit/add-button.test.js
@@ -0,0 +1,45 @@
+import React from "react";
+// import { render, fireEvent, screen } from "@testing-library/react";
+import { renderHook, act } from "@testing-library/react-hooks";
+import { TodoItemUseCase } from "../../usecase/TodoItemUseCase";
+import { RestClient } from "../../adapter/RestClient";
+import { useTodoContainer } from "../../container/useTodoContainer";
+import faker from "faker";
+test("todo-text", async () => {
+  const { result, waitForValueToChange } = renderHook(() =>
+    useTodoContainer({
+      useCase: new TodoItemUseCase(
+        new RestClient("http://todo.api.cryptobros.site/api")
+      ),
+    })
+  );
+
+  await waitForValueToChange(
+    () => {
+      return result.current.state.todoList;
+    },
+    { timeout: 2000 }
+  );
+
+  const previousLength = result.current.state.todoList.length;
+
+  const text = faker.lorem.sentence();
+  expect(result.current.state.todoText).toBe("");
+
+  await act(async () => {
+    result.current.functions.onInputChange({ target: { value: text } });
+  });
+
+  await act(async () => {
+    result.current.functions.handleButtonClick();
+  });
+
+  await waitForValueToChange(
+    () => {
+      return result.current.state.todoList;
+    },
+    { timeout: 2000 }
+  );
+
+  expect(result.current.state.todoList.length).toBe(previousLength + 1);
+});
