fe43dae6152c12f48f18b2680974004e9a45f40d
Abdulmoiz <abdulmoizahmer1996@gmail.com>
2021-11-18 08:29:18 -0700

    FIXES: House Cleaning

--- src/view/list-item/list-item.tsx
+++ src/view/list-item/list-item.tsx
@@ -4,6 +4,7 @@ import { IListItemProps } from "./IListItemProps";
 export const ListItem = function ListItem(props: IListItemProps) {
   const { description } = props.item;
   return (
+    // eslint-disable-next-line jsx-a11y/aria-role
     <li role="list-item" className="list-item">
       {description}
     </li>
