c453d5ebb7a009d334a051882685033bf3d89584
Abdulmoiz <abdulmoizahmer1996@gmail.com>
2021-12-19 01:31:34 -0700

    list tests id's are added

--- src/view/list-item/list-item.tsx
+++ src/view/list-item/list-item.tsx
@@ -3,5 +3,9 @@ import { IListItemProps } from "./IListItemProps";
 
 export const ListItem = function ListItem(props: IListItemProps) {
   const { description } = props.item;
-  return <li className="list-item">{description}</li>;
+  return (
+    <li className="list-item" data-testid="todo-list-item">
+      <span>{description}</span>
+    </li>
+  );
 };
--- src/view/list/list.tsx
+++ src/view/list/list.tsx
@@ -6,7 +6,7 @@ import { IListProps } from "./IListProps";
 export const List = function List(props: IListProps) {
   const { items } = props;
   return (
-    <ul>
+    <ul data-testid="todo-list">
       {items?.map((item: TodoItem) => {
         return <ListItem item={item} key={item.id} />;
       })}
